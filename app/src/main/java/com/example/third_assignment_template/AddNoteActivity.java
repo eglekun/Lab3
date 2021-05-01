fc0ea2ee8517217e276a8cc1eb0d12db9c8f765b
Egle <kundrotaite.egle@gmail.com>
2021-05-01 14:46:31 +0300

    Added input text checking

--- app/src/main/java/com/example/third_assignment_template/AddNoteActivity.java
+++ app/src/main/java/com/example/third_assignment_template/AddNoteActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -21,18 +22,24 @@ public class AddNoteActivity extends AppCompatActivity {
 
     public void onAddNoteClick(View view) {
         EditText txtNote = findViewById(R.id.txtNote);
+         if (txtNote.getText().toString().isEmpty()) {
+             Toast.makeText(getApplicationContext(), R.string.msgNoText, Toast.LENGTH_SHORT).show();
+         }
+         else {
+             //https://stackoverflow.com/questions/14034803/misbehavior-when-trying-to-store-a-string-set-using-sharedpreferences
+             SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+             SharedPreferences.Editor spEd = sp.edit();
+             Set<String> oldSet = sp.getStringSet("notes", new HashSet<String>());
+             Set<String> newStrSet = new HashSet<String>();
+             newStrSet.add(txtNote.getText().toString());
+             newStrSet.addAll(oldSet);
 
-        //https://stackoverflow.com/questions/14034803/misbehavior-when-trying-to-store-a-string-set-using-sharedpreferences
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        SharedPreferences.Editor spEd = sp.edit();
-        Set<String> oldSet = sp.getStringSet("notes", new HashSet<String>());
-        Set<String> newStrSet = new HashSet<String>();
-        newStrSet.add(txtNote.getText().toString());
-        newStrSet.addAll(oldSet);
+             spEd.putStringSet("notes",newStrSet);
+             spEd.apply();
 
-        spEd.putStringSet("notes",newStrSet);
-        spEd.apply();
+             finish();
+         }
 
-        finish();
+
     }
 }
